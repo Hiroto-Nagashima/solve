1c7e781f82b717df3c90d81c1d8e1aa7cfb0f80d
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-01-31 08:52:13 +0000

    [easy/login]add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   resources :users
+  
   root 'homes#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
