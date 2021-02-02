98d2d16907dec32ee81100f00b2c6feb759600e3
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-02-02 06:22:44 +0000

    [like/model/controller]add

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   resources :questions, only: [:create, :update , :destroy ,:edit]
 
   resources :comments, only: [:create, :destroy ]
+
+  resources :likes, only: [:create, :destroy ]
+
   root 'homes#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
